eb87964ebaaef2d3a8c93e853dcd7896f35ec632
MiloBytesCode <milo.zogbi@outlook.com>
2026-03-17 11:27:45 -0400

    Added some boilerplate

---
+++ src/dictionary.cpp
@@ -0,0 +1,17 @@
+/* ========================================================================= */
+// 11-zogwidrouhua-2b
+//
+// PROJECT WORD SEARCH
+//
+// The implementation for the Dictionary Class
+/* ========================================================================= */
+
+#include "dictionary.h"
+using namespace std;
+
+
+string readWord()
+{
+    return "";
+}
+
---
+++ src/dictionary.h
@@ -0,0 +1,19 @@
+/* ========================================================================= */
+// 11-zogwidrouhua-2b
+//
+// PROJECT WORD SEARCH
+//
+// The class declaraction for the Dictionary
+/* ========================================================================= */
+
+#include <string>
+
+class Dictionary
+{
+    public:
+        std::string readWord();
+        void selectionSort();
+
+    private:
+        int stuff;
+};
